30fefa4ba0972105ee65ae683d91406b9685d565
inzrb <inzrb@sina.com>
2014-12-12 11:20:14 +0800

    提交初步代码

---
+++ dist/QSwipe.min.js
@@ -0,0 +1,13 @@
+/*
+ * QSwipe 0.0.1
+ * Mobile touch slider and framework with pure page transition effects 
+ *
+ * http://isux.qq.com
+ *
+ * Copyright 2010-2014, Leon
+ *
+ * Licensed under GPL, MIT
+ *
+ * Released on: December 9, 2014
+*/
+!function(a){a.fn.picLazyLoad=function(){}}(Zepto);
